86a04c919902fdfcc261d4daecac1a47822971c5
KirkDuglas <kiryabmw@yandex.ru>
2023-11-23 19:40:01 +0300

    Создал каталоги для ДЗ второго семминара и уже выполнил первое задание

---
+++ DZ2_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// Квадрат в ОБЕ стороны.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2 || number2 == number1 * number1)
+{
+    Console.WriteLine("одно из чисел является квадратом другого");
+}
+else 
+{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого");
+}
