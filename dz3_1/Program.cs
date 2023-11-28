f477a3dbfa5a54fa4a70c71533a0a56ea6a50985
KirkDuglas <kiryabmw@yandex.ru>
2023-11-28 19:27:03 +0300

    сделал еще две домашки

--- dz3_1/Program.cs
+++ dz3_1/Program.cs
@@ -3,3 +3,24 @@
 // нет.
 // 456 => 6
 // 91 => Третьей цифры нет
+
+Console.Write("введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if (number < 10 || number < 100)
+{
+   Console.Write("Третьей цифры нет");
+} 
+    else 
+    {    int x = number;
+    while ( x > 1000 )
+    { 
+    x /= 10; 
+    }
+     int thirdDigit = x % 10;
+    Console.Write($"Третья цифра {thirdDigit}");
+    }
+       
+
+
+
--- dz4_1/Program.cs
+++ dz4_1/Program.cs
@@ -1,3 +1,35 @@
 ﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
 
 // Пример вы найдете в конце презентации к семинару.
+
+Console.Write("введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if (number < 10)
+{
+   Console.Write(number);
+} 
+    else 
+    { 
+    while ( number > 0 )
+    { 
+        
+        int x = number % 10;
+    number /= 10; 
+    if (number > 0)
+    {
+    Console.Write(x + ", ");
+    }
+    else
+    {
+        Console.Write(x);
+    }
+    }
+    }
+
+// Console.Write($"длина числа = {len}");
+// if (number < 10 || number < 100)
+
+//     Console.Write("введите число: ")
+// } 
+// int firstdigit = number 
