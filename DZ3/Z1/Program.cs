a2d2221fce520d8e99884093685df5a222be6dd7
KirkDuglas <kiryabmw@yandex.ru>
2023-11-29 18:51:49 +0300

    структурировал репозиторий, сделал 2 задачи по третьему уроку

---
+++ DZ3/Z1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int size = 10;
+int[] array = new int[size]; 
+int element = 0;
+for (int i = 0; i < array.Length; i++)
+ {
+array[i] = new Random().Next(1, 101);
+ }
+ Console.WriteLine($"Массив: [ {string.Join("; ",array)} ]");
+ for (int i = 0; i < array.Length; i++) 
+  {
+    if (array[i] >= 20 && array[i] <= 90)
+    {
+        element++;
+    }
+ }
+ Console.Write($"Число элементов в отрезке [20, 90] = {element}");
+
---
+++ DZ3/z2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+int size = 10;
+int[] array = new int[size]; 
+int element = 0;
+for (int i = 0; i < array.Length; i++)
+ {
+array[i] = new Random().Next(1, 101);
+ }
+ Console.WriteLine($"Массив: [ {string.Join("; ",array)} ]");
+ for (int i = 0; i < array.Length; i++) 
+  {
+    if (array[i] % 2 == 0)
+    {
+        element++;
+    }
+ }
+ Console.Write($"Число четных элементов в массиве = {element}");
