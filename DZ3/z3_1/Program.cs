e136887fe0e5c8a7387cc09542cfca920861795a
KirkDuglas <kiryabmw@yandex.ru>
2023-12-04 06:17:39 +0300

    учел замечания преподавателя. добавил в домашку № 3  задачу № 3_1 (поиск min&max вручную)

--- DZ3/z3_1/Program.cs
+++ DZ3/z3_1/Program.cs
@@ -8,8 +8,20 @@ for (int i = 0; i < mas.Length; i++)
 mas[i] = Math.Round(new Random().NextDouble() * new Random().Next(101), 2);
  }
  Console.WriteLine($"Массив: [ {string.Join("; ",mas)} ]");
-double max = mas.Max();
-double min = mas.Min();
+double max = mas[0];
+double min = mas[0];
+for (int i = 1; i < mas.Length; i++)
+// while (i < mas.Length)
+{
+    if (mas[i] < min)
+    {
+        min = mas[i];
+    } 
+    else if (mas[i] > max)
+    {
+        max = mas[i];
+    }
+}
 double result = Math.Round(max - min, 2);
 Console.WriteLine($"Максимальное значение массива: {max}");
 Console.WriteLine($"Минимальное значение массива: {min}");
